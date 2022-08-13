efdf416a357bb628d496f37eeb790b3cb0d6a56c
Exhum3d <sterea.victor@gmail.com>
2022-08-14 01:02:05 +0300

    implemented the BootstrapData class so we can have some initialized data when running the web application

--- src/main/java/com/dasitera/springexchange/bootstrap/BootstrapData.java
+++ src/main/java/com/dasitera/springexchange/bootstrap/BootstrapData.java
@@ -2,6 +2,7 @@ package com.dasitera.springexchange.bootstrap;
 
 import com.dasitera.springexchange.infrastructure.constant.CurrencyCode;
 import com.dasitera.springexchange.infrastructure.entity.Exchange;
+import com.dasitera.springexchange.infrastructure.entity.Transaction;
 import com.dasitera.springexchange.infrastructure.repository.ExchangeRepository;
 import com.dasitera.springexchange.infrastructure.repository.TransactionRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -9,6 +10,9 @@ import org.springframework.stereotype.Component;
 
 /**
  * BootstrapData class is resposible for initializing data
+ *
+ * @Component annotations tells spring that the class is a
+ * spring managed component
  */
 @Component
 public class BootstrapData implements CommandLineRunner {
@@ -26,10 +30,23 @@ public class BootstrapData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        Exchange rate1 = new Exchange();
-        rate1.setFromCurrency(CurrencyCode.USD);
-        rate1.setToCurrency(CurrencyCode.RON);
-        rate1.setRate(4.76);
+        Exchange rate1 = new Exchange(CurrencyCode.USD, CurrencyCode.RON, 4.76);
+        Transaction transaction1 = new Transaction("Ion", "Tudor", "itudor@gmail.com", 40);
+
+        transaction1.setExchange(rate1);
+
+        exchangeRepository.save(rate1);
+        transactionRepository.save(transaction1);
+
+
+        Exchange rate2 = new Exchange(CurrencyCode.EUR, CurrencyCode.RON, 4.89);
+        Transaction transaction2 = new Transaction("Mariana", "Mihalache", "mmihalache@gmail.com", 24);
+
+        transaction2.setExchange(rate2);
+
+        exchangeRepository.save(rate2);
+        transactionRepository.save(transaction2);
+
 
     }
 }
